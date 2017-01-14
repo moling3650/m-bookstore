5014a3c3dd00a07176017ec9ebe8b93c2bdbfbdf
moling3650 <365024424@qq.com>
2017-01-14 08:37:14 +0800

    导入静态资源服务并测试

--- app.js
+++ app.js
@@ -3,16 +3,26 @@
 let koa = require('koa')
 let controller = require('koa-route')
 let views = require('co-views')
+let koa_static = require('koa-static-server')
 
+// 初始化服务器
 let app = koa()
+
+// 初始化服务端渲染器
 let render = views('./view', {
     map: {
         html: 'ejs'
     }
 })
 
+// 初始化静态资源服务器
+app.use(koa_static({
+    rootDir: './static/',
+    rootPath: '/static/',
+    maxage: 0
+}))
 
-app.use(controller.get('/', function*() {
+app.use(controller.get('/', function* () {
     this.set('Cache-Control', 'no-cache')
     this.body = yield render('index', {name: 'ejs'})
 }))
---
+++ static/main.js
@@ -0,0 +1,3 @@
+'use strict'
+
+alert('static server ok')
