a6886f5a817449d39e321fdf1243bebe0be48e3e
moling3650 <365024424@qq.com>
2017-01-15 16:45:03 +0800

    模拟从服务端取得数据，动态生成页面

--- static/js/page/index.js
+++ static/js/page/index.js
@@ -1,1 +1,15 @@
 'use strict'
+$.get('/ajax/', (d) => {
+  new Vue({
+    el: '#root',
+    data: {
+      top: d.items[0].data.data,
+      hot: d.items[1].data.data,
+      recommend: d.items[2].data.data,
+      female: d.items[3].data.data,
+      male: d.items[4].data.data,
+      free: d.items[5].data.data,
+      topic: d.items[6].data.data
+    }
+  })
+}, 'json')
