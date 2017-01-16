81d3730513a69848eb3c0112c6c01307ec35619c
moling3650 <365024424@qq.com>
2017-01-16 14:42:06 +0800

    书籍详情页面模拟接收服务端数据

--- static/js/page/book.js
+++ static/js/page/book.js
@@ -1,1 +1,15 @@
 'use strict'
+let match = location.search.match(/[&\?]id=([^&]+)/)
+let id = match ? match.pop() : ''
+
+$.get(`/ajax/book?id=${id}`, (d) => {
+  new Vue({
+    el: '#root',
+    data: d,
+    methods: {
+      readBook () {
+
+      }
+    }
+  })
+}, 'json')
